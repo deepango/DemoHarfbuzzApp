f4c1dc838086f4cd71d58e81c5e36ffc98ef33f7
digib0y <code.deepanshu@gmail.com>
2017-06-06 10:47:21 +0000

    Made changes Bitmap

--- MainView.cpp
+++ MainView.cpp
@@ -3,68 +3,85 @@
 MainView::MainView(const BRect &frame)
 	:	BView(frame, "textview", B_FOLLOW_ALL, B_WILL_DRAW)
 {
-	// the font rasterizing library
-    FreeTypeLib lib;
-    //clock_t begin, end;
-
-    HBShaper latinShaper("DejaVuSerif.ttf", &lib);
-    HBShaper arabicShaper("amiri-regular.ttf", &lib);
-    HBShaper russianShaper("DejaVuSerif.ttf", &lib);
-    HBShaper hanShaper("fireflysung.ttf", &lib);
-    HBShaper hindiShaper("Sanskrit2003.ttf", &lib);
-
-    latinShaper.init();
-    arabicShaper.init();
-    russianShaper.init();
-    hanShaper.init();
-    hindiShaper.init();
-
-    HBText hbt1 = {
-        "ficellé fffffi. VAV.",
-        "fr",
-        HB_SCRIPT_LATIN,
-        HB_DIRECTION_LTR
-    };
-
-    HBText hbt2 = {
-        "تسجّل يتكلّم",
-        "ar",
-        HB_SCRIPT_ARABIC,
-        HB_DIRECTION_RTL
-    };
-
-    HBText hbt3 = {
-        "Дуо вёжи дёжжэнтиюнт ут",
-        "ru",
-        HB_SCRIPT_CYRILLIC,
-        HB_DIRECTION_LTR
-    };
-
-    HBText hbt4 = {
-        "緳 踥踕",
-        "ch",
-        HB_SCRIPT_HAN,
-        HB_DIRECTION_TTB
-    };
-
-    HBText hbt5 = {
-        "हालाँकि प्रचलित रूप पूजा",
-        "hi",
-        HB_SCRIPT_DEVANAGARI,
-        HB_DIRECTION_LTR
-    };
-
-    latinShaper.addFeature(HBFeature::KerningOn);
 }
 
 
 void
-MainView::Draw(BRect update)
+MainView::Draw(BRect updateRect)
 {
-	BFont font;
-	font.SetSize(18.0);
-	
-	SetFont(&font);
-	
-	DrawString("This is some text", BPoint(50,50));
+    // Get the current size of the view
+    BRect bounds = Bounds();
+
+    // Use it to allocate a BBitmap on the
+    // stack that is the size of the view
+    BBitmap bitmap(bounds, B_RGBA32);
+
+    // Write the demo text into the bitmaps memory:
+    _DrawText(&bitmap);
+
+    // Draw the bitmap on the view's surface
+    // see https://www.haiku-os.org/legacy-docs/bebook/BView.html#BView_Drawing
+    DrawBitmap(&bitmap, updateRect, updateRect);
+}
+
+void
+MainView::_DrawText(const BBitmap* bitmap)
+{
+    // the font rasterizing library
+    FreeTypeLib lib;
+
+    HBShaper latinShaper("DejaVuSerif.ttf", &lib);
+    HBShaper arabicShaper("amiri-regular.ttf", &lib);
+    HBShaper russianShaper("DejaVuSerif.ttf", &lib);
+    HBShaper hanShaper("fireflysung.ttf", &lib);
+    HBShaper hindiShaper("Sanskrit2003.ttf", &lib);
+
+    latinShaper.init();
+    arabicShaper.init();
+    russianShaper.init();
+    hanShaper.init();
+    hindiShaper.init();
+
+    HBText hbt1 = {
+        "ficellé fffffi. VAV.",
+        "fr",
+        HB_SCRIPT_LATIN,
+        HB_DIRECTION_LTR
+    };
+
+    HBText hbt2 = {
+        "تسجّل يتكلّم",
+        "ar",
+        HB_SCRIPT_ARABIC,
+        HB_DIRECTION_RTL
+    };
+
+    HBText hbt3 = {
+        "Дуо вёжи дёжжэнтиюнт ут",
+        "ru",
+        HB_SCRIPT_CYRILLIC,
+        HB_DIRECTION_LTR
+    };
+
+    HBText hbt4 = {
+        "緳 踥踕",
+        "ch",
+        HB_SCRIPT_HAN,
+        HB_DIRECTION_TTB
+    };
+
+    HBText hbt5 = {
+        "हालाँकि प्रचलित रूप पूजा",
+        "hi",
+        HB_SCRIPT_DEVANAGARI,
+        HB_DIRECTION_LTR
+    };
+    latinShaper.addFeature(HBFeature::KerningOn);
+
+    // Draw the glyphs of each shaper, pass the bitmap to draw on
+    latinShaper.drawText(hbt1, bitmap, 20, 320);
+    arabicShaper.drawText(hbt2, bitmap, 20, 220);
+    russianShaper.drawText(hbt3, bitmap, 20, 120);
+    hanShaper.drawText(hbt4, bitmap, 700, 380);
+    hindiShaper.drawText(hbt5, bitmap, 20, 20);
 }
--- hbshaper.h
+++ hbshaper.h
@@ -37,7 +37,7 @@ class HBShaper {
         ~HBShaper();
 
         void init();
-        vector<gl::Mesh*> drawText(HBText& text, float x, float y);
+        void HBShaper::drawText(HBText& text, const BBitmap* bitmap, float x, float y);
         void addFeature(hb_feature_t feature);
 
     private:
@@ -59,95 +59,86 @@ void HBShaper::addFeature(hb_feature_t feature) {
     features.push_back(feature);
 }
 
-vector<gl::Mesh*> HBShaper::drawText(HBText& text, float x, float y) {
-    vector<gl::Mesh*> meshes;
+void
+HBShaper::drawText(HBText& text, const BBitmap* bitmap, float x, float y)
+{
+    // Get a pointer to the BBitmaps memory buffer
+    uint8* bits = (uint8*)bitmap->Bits();
+    // There are this many bytes in one row
+    uint32 bytesPerRow = bitmap->BytesPerRow();
 
-    hb_buffer_reset(buffer);
+    // You don't need the GL meshes anymore, you can copy the rasterized
+    // glyph directly into the BBitmap's memory. You may have to do a
+    // color space conversion, i.e. the glyph's data may be gray scale,
+    // but the BBitmap's buffer is BGRA with 8 bits per chanel.
 
-    hb_buffer_set_direction(buffer, text.direction);
-    hb_buffer_set_script(buffer, text.script);
-    hb_buffer_set_language(buffer, hb_language_from_string(text.language.c_str(), text.language.size()));
-    size_t length = text.data.size();
+    // Here is the original code from the test app
+    hb_buffer_reset(buffer);
 
-    hb_buffer_add_utf8(buffer, text.c_data(), length, 0, length);
+    hb_buffer_set_direction(buffer, text.direction);
+    hb_buffer_set_script(buffer, text.script);
+    hb_buffer_set_language(buffer,
+        hb_language_from_string(text.language.c_str(),
+        text.language.size()));
+    size_t length = text.data.size();
 
-    // harfbuzz shaping
-    hb_shape(font, buffer, features.empty() ? NULL : &features[0], features.size());
+    hb_buffer_add_utf8(buffer, text.c_data(), length, 0, length);
 
-    unsigned int glyphCount;
-    hb_glyph_info_t *glyphInfo = hb_buffer_get_glyph_infos(buffer, &glyphCount);
-    hb_glyph_position_t *glyphPos = hb_buffer_get_glyph_positions(buffer, &glyphCount);
+    // harfbuzz shaping
+    hb_shape(font, buffer, features.empty()
+        ? NULL : &features[0], features.size());
 
-    for(int i = 0; i < glyphCount; ++i) {
-        Glyph* glyph = lib->rasterize(face, glyphInfo[i].codepoint);
+    unsigned int glyphCount;
+    hb_glyph_info_t *glyphInfo = hb_buffer_get_glyph_infos(
+        buffer, &glyphCount);
+    hb_glyph_position_t *glyphPos
+        = hb_buffer_get_glyph_positions(buffer, &glyphCount);
 
-        int twidth = pow(2, ceil(log(glyph->width)/log(2)));
-        int theight = pow(2, ceil(log(glyph->height)/log(2)));
+    for (int i = 0; i < glyphCount; ++i) {
+        Glyph* glyph = lib->rasterize(face, glyphInfo[i].codepoint);
 
-        auto tdata = new unsigned char[twidth * theight] ();
+        // From the original code...
+        // The other variables are probably no longer needed:
+        float xa = (float) glyphPos[i].x_advance / 64;
+        float ya = (float) glyphPos[i].y_advance / 64;
+        float xo = (float) glyphPos[i].x_offset / 64;
+        float yo = (float) glyphPos[i].y_offset / 64;
+        float x0 = x + xo + glyph->bearing_x;
+        float y0 = floor(y + yo + glyph->bearing_y);
 
-        for(int iy = 0; iy < glyph->height; ++iy) {
-            memcpy(tdata + iy * twidth, glyph->buffer + iy * glyph->width, glyph->width);
-        }
+        // Figure out the offset into the memory of the bitmap
+        // for x and y:
+        // This probaly needs to be changed to use x0 and y0 instead
+        // of x and y.
+        uint8* pixel = bits + (int32)y * bytesPerRow + (int32)x * 4;
 
-#ifdef DEBUG
-        for(int iy = 0; iy < glyph->height; ++iy) {
-            for(int ix = 0; ix < glyph->width; ++ix) {
-                int c = (int) glyph->buffer[iy * glyph->width + ix];
-                cout << (c == 255 ? '#' : '`');
-            }
-            cout << endl;
-        }
-        cout << endl;
-#endif
+        // Copy the rasterized glyph into the bitmap buffer
+        for (int iy = 0; iy < glyph->height; ++iy) {
+            uint8* p = pixel;
+            // Offset into the rasterized glyph buffer:
+            uint8* g = glyph->buffer + iy * glyph->width;
 
-        float s0 = 0.0;
-        float t0 = 0.0;
-        float s1 = (float) glyph->width / twidth;
-        float t1 = (float) glyph->height / theight;
-        float xa = (float) glyphPos[i].x_advance / 64;
-        float ya = (float) glyphPos[i].y_advance / 64;
-        float xo = (float) glyphPos[i].x_offset / 64;
-        float yo = (float) glyphPos[i].y_offset / 64;
-        float x0 = x + xo + glyph->bearing_x;
-        float y0 = floor(y + yo + glyph->bearing_y);
-        float x1 = x0 + glyph->width;
-        float y1 = floor(y0 - glyph->height);
+            // Copy one row of the glyph data while doing a
+            // "color space conversion":
+            for (int ix = 0; iy < glyph->width; ++ix) {
+                p[0] = *g;
+                p[1] = *g;
+                p[2] = *g;
+                p[3] = 255;
 
-        gl::Vertex* vertices = new gl::Vertex[4];
-        vertices[0] = { x0,y0, s0,t0 };
-        vertices[1] = { x0,y1, s0,t1 };
-        vertices[2] = { x1,y1, s1,t1 };
-        vertices[3] = { x1,y0, s1,t0 };
+                p += 4; // four bytes per pixel
+                g += 1; // probably just one byte per pixel
+            }
+            // Next row in the bitmap's buffer:
+            pixel += bytesPerRow;
+        }
 
-        unsigned short* indices = new unsigned short[6];
-        indices[0] = 0; indices[1] = 1;
-        indices[2] = 2; indices[3] = 0;
-        indices[4] = 2; indices[5] = 3;
+        // Advance x and y for the next glyph
+        x += xa;
+        y += ya;
 
-        gl::Mesh* m = new gl::Mesh;
-
-        m->indices = indices;
-        m->textureData = tdata;
-
-        // don't do this!! use atlas texture instead
-        m->textureId = gl::getTextureId(twidth, theight);
-
-        m->vertices = vertices;
-        m->nbIndices = 6;
-        m->nbVertices = 4;
-
-        gl::uploadTextureData(m->textureId, twidth, theight, tdata);
-
-        meshes.push_back(m);
-
-        x += xa;
-        y += ya;
-
-        lib->freeGlyph(glyph);
-    }
-
-    return meshes;
+        lib->freeGlyph(glyph);
+    }
 }
 
 void HBShaper::init() {
